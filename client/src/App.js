f43ee094513aac68d881a029c242667855c2f306
ghildreth <gabrielantonhildreth@gmail.com>
2018-06-18 15:12:19 -0700

    add toggle dropdown menu to goal tracker

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    goals: "",
+    }
+  componentDidMount(){
+    let goalTypes = document.getElementsByClassName('dropdown-content');
+    console.log('works?', goalTypes[0]);
+    this.setState({goals: goalTypes[0]})
+    
+  }
   render() {
+
+    function handleClick(e){
+      e.preventDefault();
+      console.log(this.state.goals.classList.toggle('open'));
+
+
+    }
+    console.log('state', this.state)
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Suppp bro</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form>
+          <label htmlFor="goal">What do you want to-"done"?</label>
+            <input type="text" name="goal" id="goal"/>
+              <div className="dropdown">
+                <span onClick={handleClick.bind(this)}>Mouse over me</span>
+                  <div className="dropdown-content">
+                    <p>Hello World!</p>
+                  </div>
+              </div>
+        </form>
+        
+        <div className="columns">
+          <div className="daily">
+            <h2>Daily</h2>
+          </div>
+          <div className="core">
+            <h2>Core</h2>
+          </div>
+          <div className="short-term">
+            <h2>Short-term</h2>
+          </div>
+          <div className="long-term">
+            <h2>Long-term</h2>
+          </div>
+        </div>
       </div>
     );
   }
